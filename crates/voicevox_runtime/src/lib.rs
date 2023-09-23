7b5da190a1cea73fd2ba7c324276dbce0fcd35a6
Ryo Yamashita <qryxip@gmail.com>
2023-09-23 18:03:11 +0900

    [wip] First implementation

--- crates/voicevox_runtime/src/lib.rs
+++ crates/voicevox_runtime/src/lib.rs
@@ -1,1 +1,18 @@
+mod backends;
+mod capi;
+mod error;
 
+pub use self::error::Error;
+
+pub type Result<T> = std::result::Result<T, self::error::Error>;
+
+pub struct ModelRuntime {
+    backend: backends::onnxruntime::Backend,
+}
+
+impl ModelRuntime {
+    pub fn new() -> crate::Result<Self> {
+        let backend = backends::onnxruntime::Backend::new()?;
+        Ok(Self { backend })
+    }
+}
